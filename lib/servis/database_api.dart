9ead129b12c90b213595149fafb3c4d8119baef9
cihanerdogan <cihanerdogan35@gmail.com>
2020-12-14 16:03:51 +0300

    ilk

--- lib/servis/database_api.dart
+++ lib/servis/database_api.dart
@@ -1,8 +1,7 @@
 import 'package:ilkDenemem/model/kategori.dart';
-import 'package:ilkDenemem/servis/soyut_database_api.dart';
 import 'package:http/http.dart' as http;
 
-class DbRestApi implements DbApi {
+class DbRestApi {
   String url = "http://yeni.bursa.com.tr/json.php?part=places";
   @override
   Future<Kategori> mekanGetir(String id) {
